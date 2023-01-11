5197ce2bf98677b80b129159488b2ade52e72694
JavAv11 <Javcoria01@gmail.com>
2023-01-11 16:05:07 -0600

    Mapa

---
+++ PL/Controllers/CineController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace PL.Controllers
+{
+    public class CineController : Controller
+    {
+        [HttpGet]
+        public ActionResult GettAll()
+        {
+            //ML.Cine cine = new ML.Cine();
+
+            //using(var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri("  https://www.google.com/maps/embed/v1/");
+            //    var responseTask = client.GetAsync("MAP_MODE?key=AIzaSyBJB98-M9YPAuSyEZgnPUrKPp3djyA4364&parameters");
+            //    responseTask.Wait();
+
+            //    var result = responseTask.Result;
+            //    if(result.IsSuccessStatusCode)
+            //    {
+            //        var readTask = result.Content.ReadAsStringAsync();
+            //        dynamic resulJSON = JsonObject.Parse(readTask.Result.ToString());
+            //        responseTask.Wait();
+                    
+            //    }
+            //}
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+}
